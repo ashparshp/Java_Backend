088aa8bf47331ede9d8da43b5b4d60bd9b83a347
Ashparshp <ashparsh.connects@gmail.com>
2025-03-08 13:04:45 +0530

    Add Car class with constructors and instance initialization block; create CarMain for testing

--- Java/01_Classes/Classes/src/Car.java
+++ Java/01_Classes/Classes/src/Car.java
@@ -3,6 +3,25 @@ public class Car {
     String color;
     int horsePower;
 
+    // Instance Initialization Block: It is always called whenever an object is created.
+    {
+        this.model = "BMW";
+        this.color = "Black";
+        this.horsePower = 200;
+        System.out.println("Instance Initialization Block called");
+    }
+
+    public Car() {
+        System.out.println("Car constructor called");
+    }
+
+    public Car(String model, String color, int horsePower) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+        System.out.println("Car constructor called");
+    }
+
     public void startCar() {
         System.out.println("Vehicle started");
     }
