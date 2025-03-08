847febd9d62cc76614c5635c9a8b1a1f513a287b
Ashparshp <ashparsh.connects@gmail.com>
2025-03-08 12:40:51 +0530

    Refactor EmployeeMain to demonstrate default constructor and constructor overloading

--- Java/01_Classes/Classes/src/EmployeeMain.java
+++ Java/01_Classes/Classes/src/EmployeeMain.java
@@ -1,19 +1,23 @@
 public class EmployeeMain {
     public static void main(String[] args) {
         
-        Employee employee  = new Employee();
         /*
+        // Default constructor
+        Employee employee  = new Employee();
         employee.firstName = "John";
         employee.lastName = "Smith";
         employee.age = 24;
         employee.gender = 'M';
         */
 
+        // Default constructor with default values
+        Employee employee = new Employee();
         System.out.println(employee.getFirstName());
         System.out.println(employee.getLastName());
         System.out.println(employee.getAge());
         System.out.println(employee.getGender());
 
+        // Constructor overloading
         Employee employee2 = new Employee("Ram", "Ji", (byte) 25, 'M');
         System.out.println(employee2.getFirstName());
         System.out.println(employee2.getLastName());
