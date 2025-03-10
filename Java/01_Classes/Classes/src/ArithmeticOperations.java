50d12e8c20915f306ebfe020f469d7175bc413f7
Ashparshp <ashparsh.connects@gmail.com>
2025-03-10 12:10:23 +0530

    Static

--- Java/01_Classes/Classes/src/ArithmeticOperations.java
+++ Java/01_Classes/Classes/src/ArithmeticOperations.java
@@ -1,6 +1,30 @@
 public class ArithmeticOperations {
 
+    double pi = 3.14;
+    static final double PI = 3.14;
+
+    public static void main(String[] args) {
+
+        ArithmeticOperations arithmeticOperations = new ArithmeticOperations();
+        int sum = arithmeticOperations.sum(5, 3);
+        System.out.println("Sum: " + sum);
+
+
+        int result = addition(5, 3);
+        System.out.println("Result: " + result);
+
+        System.out.println("PI: " + arithmeticOperations.pi);
+        System.out.println("PI Static: " + PI);
+    }
+
     public int sum(int a, int b) {
+        System.err.println("PI: " + pi);
+        System.err.println("PI Static: " + PI);
         return a + b;
     }
+
+    public static  int addition(int a, int b) {
+        return a + b;
+    }
+
 }
